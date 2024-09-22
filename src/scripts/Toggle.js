1004437d300daf8ba8dd9ef5b5f407e25de8a1ed
Soy-Sebas <sebastian.gutierrez.guayacundo@gmail.com>
2024-09-22 17:26:31 -0500

    New Header and Organization

--- src/scripts/Toggle.js
+++
@@ -1,7 +0,0 @@
-
-const menuToggle = document.getElementById('menu-toggle');
-const mobileMenu = document.getElementById('menu');
-
-menuToggle.addEventListener('click', () => {
-	mobileMenu.classList.toggle('hidden'); // Alterna la clase hidden para mostrar/ocultar el menú
-});
